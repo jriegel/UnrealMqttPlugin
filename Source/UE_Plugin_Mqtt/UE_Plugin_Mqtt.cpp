24a84dda2f2620405aac43d5ccbd32d48fc88a87
Juergen Riegel <mail@juergen-riegel.net>
2023-01-17 10:23:52 +0100

    Initial commit

---
+++ Source/UE_Plugin_Mqtt/UE_Plugin_Mqtt.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_Plugin_Mqtt.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_Plugin_Mqtt, "UE_Plugin_Mqtt" );
---
+++ Source/UE_Plugin_Mqtt/UE_Plugin_MqttGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_Plugin_MqttGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_PLUGIN_MQTT_API AUE_Plugin_MqttGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
