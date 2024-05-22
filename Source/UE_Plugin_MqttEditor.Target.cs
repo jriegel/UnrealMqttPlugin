422e0dd1d121a731ae16ce50698ad6003ed0965b
Juergen Riegel <mail@juergen-riegel.net>
2024-05-22 14:52:45 +0200

    Modernise for 5.3

--- Source/UE_Plugin_MqttEditor.Target.cs
+++ Source/UE_Plugin_MqttEditor.Target.cs
@@ -8,8 +8,8 @@ public class UE_Plugin_MqttEditorTarget : TargetRules
 	public UE_Plugin_MqttEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 		ExtraModuleNames.Add("UE_Plugin_Mqtt");
 	}
 }
